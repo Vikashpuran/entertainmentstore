c0e88de9c00b557910cc9403609fb5898ac04fe1
Vikashpuran <73464682+Vikashpuran@users.noreply.github.com>
2022-08-28 00:13:36 +0530

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+ const Header = () => {
+  return (
+    <div className="App">
+    <span  onClick = {() => window.scroll(0,0)} className="header">📽️Entertainment Hub🎬</span>
+    </div>
+  )
+}
+
+export default Header;
+
